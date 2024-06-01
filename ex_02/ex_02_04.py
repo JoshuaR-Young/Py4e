619f26c31bc071216e12073d23f33a8ceb1a5518
Joshua Young <Joshuareiyoung@gmail.com>
2024-05-31 22:45:17 -0400

    Add files via upload

---
+++ ex_02/ex_02_04.py
@@ -0,0 +1,14 @@
+#Assignment statement
+width = 17
+height = 12.0
+
+#Expression Evaluation
+expr1 = width//2
+expr2 =width/2.0
+expr3 =height/3
+expr4 =1 + 2 * 5
+
+expr1 = print(f'Expression: width // 2 -> Value: {expr1}, Type: {type(expr1)}')
+expr2 = print(f'Expression: width / 2.0 -> Value: {expr2}, Type: {type(expr2)}')
+expr3 = print(f'Expression: height / 3 -> Value: {expr3}, Type: {type(expr3)}')
+expr4 = print(f'Expression: 1 + 2 * 5 -> Value: {expr4} Type: {type(expr4)}')

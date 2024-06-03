2a1e3b65ae06775f986596b41cea0e6d5c28603c
Joshua Young <Joshuareiyoung@gmail.com>
2024-06-02 22:34:39 -0400

    Add files via upload

---
+++ ex_04/ex_04_02.py
@@ -0,0 +1,18 @@
+repeat_lyrics()
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+'''
+Traceback (most recent call last):
+  File "d:\Coding Projects\python projects\py4e\ex_04\ex_04_02.py", line 1, in <module>
+    repeat_lyrics()
+    ^^^^^^^^^^^^^
+NameError: name 'repeat_lyrics' is not defined
+'''

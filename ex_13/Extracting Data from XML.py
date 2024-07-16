0070d5ca5eb9e47bb949498cfcdb3b2ebb656277
Joshua Young <Joshuareiyoung@gmail.com>
2024-07-15 20:30:13 -0400

    Extracting Data from XML.py

--- ex_13/Extracting Data from XML.py
+++ ex_13/Extracting Data from XML.py
@@ -1,11 +1,11 @@
+import urllib.request
 import xml.etree.ElementTree as ET
 
-# Update the file_path variable with the actual path to your XML file
-file_path = 'comments_2054099.xml'
+url = 'https://py4e-data.dr-chuck.net/comments_2054099.xml'
 
-# Read the data from the file
-with open(file_path, 'r') as file:
-    data = file.read()
+# Fetch the data from the URL
+response = urllib.request.urlopen(url)
+data = response.read().decode('utf-8')
 
 # Print the data length
 data_length = len(data)
@@ -16,6 +16,5 @@ tree = ET.fromstring(data)
 counts = tree.findall('.//count')
 nums = [int(count.text) for count in counts]
 
-# Print the results
 print('Count:', len(nums))
 print('Sum:', sum(nums))
